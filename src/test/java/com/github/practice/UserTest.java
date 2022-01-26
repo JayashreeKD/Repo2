b7e00d743507973f9606607ca9d32a11d5f6b03c
admin <admin@DESKTOP-7E5APTE>
2022-01-26 19:52:54 +0530

    user 1 first comit

---
+++ src/test/java/com/github/practice/UserTest.java
@@ -0,0 +1,14 @@
+package com.github.practice;
+
+import org.testng.annotations.Test;
+
+public class UserTest {
+	
+	
+	@Test
+	public void usertest()
+	{
+		System.out.println("user 1 first comit");
+	}
+
+}
